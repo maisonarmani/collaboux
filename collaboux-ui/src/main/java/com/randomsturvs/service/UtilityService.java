bccb699a2884c901e35ef90cd962d24c09002968
Maison Armani <masonarmani38@gmail.com>
2020-05-14 14:36:59 +0100

    added caching config

--- collaboux-ui/src/main/java/com/randomsturvs/service/UtilityService.java
+++ collaboux-ui/src/main/java/com/randomsturvs/service/UtilityService.java
@@ -2,6 +2,7 @@ package com.randomsturvs.service;
 
 import com.randomsturvs.collaboux.services.GenreService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -12,6 +13,7 @@ public class UtilityService {
     @Autowired
     private GenreService genreService;
 
+    @Cacheable(value="genres")
     public List<String> getAllGenre(){
         return  genreService.getAll();
     }
